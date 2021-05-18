2076b3432b236535a82b6719d356f4cc90fa463f
Mattan1999 <mattias470@gmail.com>
2021-05-18 15:38:13 +0200

    Snyggade till koden i Mail.vue
    - Gjorde så man enkelt kan läsa vad som står i "Email" variabeln.

--- src/views/Mail.vue
+++ src/views/Mail.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="mail">
-         
         <label>Name:
             <input type="text" placeholder="name" v-model="name">
         </label>
@@ -17,33 +16,64 @@
 </template>
 
 <script>
-
 export default {
-        data() {
-            var Email = { send: function (a) { return new Promise(function (n) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new this.XDomainRequest).open(e, n) : t = null, t } }
-            return{
+    data() {
+        var Email = { 
+            send: function (a) { 
+                return new Promise(function (n) { 
+                    a.nocache = Math.floor(1e6 * Math.random() + 1), 
+                    a.Action = "Send"; 
+                    var t = JSON.stringify(a); 
+                    Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { 
+                        n(e) 
+                    }) 
+                }) 
+            }, 
+            ajaxPost: function (e, n, t) {
+                var a = Email.createCORSRequest("POST", e); 
+                a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), 
+                a.onload = function () { 
+                    var e = a.responseText; 
+                    null != t && t(e) 
+                }, 
+                a.send(n) 
+            }, 
+            ajax: function (e, n) {
+                var t = Email.createCORSRequest("GET", e); 
+                t.onload = function () { 
+                    var e = t.responseText; 
+                    null != n && n(e) 
+                }, 
+                t.send() 
+            }, 
+            createCORSRequest: function (e, n) { 
+                var t = new XMLHttpRequest; 
+                return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new this.XDomainRequest).open(e, n) : t = null, t 
+            } 
+        }
+        return{
             email: "",
             msg: "",
             name: "",
             Email
-            }
-        },
-        methods: {
-            sendEmail(email, msg, name) {
-                this.Email.send({
-                    Host : "mail.gmx.com",
-                    Username : "webstore496@gmx.com",
-                    Password : "&KX*3aFz8vzo",
-                    To : email,
-                    From : "webstore496@gmx.com",
-                    Subject : "Importaint message for Mr."+name+"!",
-                    Body : msg,
-                }).then(
+        }
+    },
+    methods: {
+        sendEmail(email, msg, name) {
+            this.Email.send({
+                Host : "mail.gmx.com",
+                Username : "webstore496@gmx.com",
+                Password : "&KX*3aFz8vzo",
+                To : email,
+                From : "webstore496@gmx.com",
+                Subject : "Importaint message for Mr. "+name+"!",
+                Body : msg,
+            }).then(
                 message => alert(message),
                 console.log('message: '+msg+' email: '+email+' name: '+name)
-                );
-            }
-        },
-  name: "Mail",
+            );
+        }
+    },
+    name: "Mail",
 };
 </script>
