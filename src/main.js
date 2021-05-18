beb009d3680b4a2bfccda340bb928e7030ba623d
Cassandra Holmquist <71176486+cassandraholmquist@users.noreply.github.com>
2021-05-18 14:31:20 +0200

    Lagt till dependencies (bootstrap4, bootstrapVue, sass) Gör en npm install efter pull.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './main.scss'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
