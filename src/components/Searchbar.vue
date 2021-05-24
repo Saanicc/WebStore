b89f5cf48c509b8509237ec9dc5cd9e01980e463
Mattan1999 <mattias470@gmail.com>
2021-05-24 13:28:54 +0200

    Gjorde produktsidan responsiv samt styleade sökrutan

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="searchbar">
-    <input v-model="searchQuery" placeholder="Search product">
+    <input v-model="searchQuery" placeholder="Sök efter produkter...">
   </div>
 </template>
 
@@ -23,3 +23,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#searchbar > input {
+  border: none;
+  box-shadow: #00000030 1px 2px 4px;
+  padding: 8px 10px;
+  width: 90%;
+}
+
+@media screen and (min-width: 575px) {
+  #searchbar > input {
+    width: 500px;
+  }
+}
+</style>
