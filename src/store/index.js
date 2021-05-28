ae46f9f39e9c117ab9e603563e467381379c5f9e
Sebastian_Warvsten <sebastian.warvsten@iths.se>
2021-05-28 17:20:20 +0200

    Gorde filterfunktionen DRY

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import Data from "../../public/data.json";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Data from '../../public/data.json'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
@@ -10,78 +10,77 @@ export default new Vuex.Store({
     filteredProducts: [],
     products: Data.phones,
     searchQuery: null,
-    wishList: [],
+    wishList: []
   },
   mutations: {
     filterProductsByCategory(state, filteredProducts) {
-      state.filteredProducts = filteredProducts;
+      state.filteredProducts = filteredProducts
       if (state.filteredProducts.length > 0 && !state.searchQuery) {
-        state.products = Data.phones.filter((product) => {
-          return state.filteredProducts.find((c) => product.name.match(c));
-        });
+        this.commit('checkboxFilter')
       } else {
         if (!state.searchQuery) {
-          state.products = Data.phones;
+          state.products = Data.phones
         } else {
-          state.products = Data.phones.filter((product) => {
-            return state.searchQuery
-              .toLowerCase()
-              .split(" ")
-              .every((c) => product.name.toLowerCase().includes(c));
-          });
+          this.commit('lettersFilter')
         }
       }
     },
     searchQuery(state, searchQuery) {
-      state.searchQuery = searchQuery;
+      state.searchQuery = searchQuery
       if (state.searchQuery) {
-        state.products = Data.phones.filter((product) => {
-          return state.searchQuery
-            .toLowerCase()
-            .split(" ")
-            .every((c) => product.name.toLowerCase().includes(c));
-        });
+        this.commit('lettersFilter')
       } else {
-        if(state.filteredProducts.length < 1){
-          state.products = Data.phones;
+        if (state.filteredProducts.length < 1) {
+          state.products = Data.phones
         } else {
-          state.products = Data.phones.filter((product) => {
-            return state.filteredProducts.find((c) => product.name.match(c));
-          });
+          this.commit('checkboxFilter')
         }
       }
     },
+    checkboxFilter(state) {
+      state.products = Data.phones.filter((product) => {
+        return state.filteredProducts.find((c) => product.name.match(c))
+      })
+    },
+    lettersFilter(state) {
+      state.products = Data.phones.filter((product) => {
+        return state.searchQuery
+          .toLowerCase()
+          .split(' ')
+          .every((c) => product.name.toLowerCase().includes(c))
+      })
+    },
     addToCart(state, product) {
       if (state.cart.includes(product)) {
-        product.quantity += 1;
+        product.quantity += 1
       } else {
-        product.quantity = 1;
-        state.cart.push(product);
+        product.quantity = 1
+        state.cart.push(product)
       }
     },
     removeFromCart(state, product) {
       for (let i = 0; i < state.cart.length; i++) {
         if (state.cart[i].id === product.id) {
-          state.cart.splice(i, 1);
-          break;
+          state.cart.splice(i, 1)
+          break
         }
       }
     },
     clearCart(state) {
-      state.cart = [];
+      state.cart = []
     },
     addToWishList(state, product) {
-      state.wishList.push(product);
+      state.wishList.push(product)
     },
     removeFromWishList(state, product) {
-      console.log(state.wishList);
+      console.log(state.wishList)
       for (let i = 0; i < state.wishList.length; i++) {
         if (state.wishList[i].id === product.id) {
-          state.wishList.splice(i, 1);
+          state.wishList.splice(i, 1)
         }
       }
-    },
+    }
   },
   actions: {},
-  modules: {},
-});
+  modules: {}
+})
