22d23d205ab33ad0943f9413eae4778a56e47ef4
Mattan1999 <mattias470@gmail.com>
2021-05-29 18:15:09 +0200

    Snyggade till designen på kundvagnen

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -6,7 +6,7 @@
       <table class="table is-striped">
         <thead>
           <tr>
-            <th>Namn</th>
+            <th>Produkt</th>
             <th>Antal</th>
             <th>Pris</th>
             <th></th>
@@ -14,7 +14,14 @@
         </thead>
         <tbody>
           <tr class="table-item" v-for="product in products" :key="product.id">
-            <td>{{ product.name }}</td>
+            <td>
+              <img
+                :src="product.img"
+                width="100px"
+                style="margin-right: 0.5rem;"
+              />
+              {{ product.name }}
+            </td>
             <td>
               <div class="product-quantity">
                 <button @click="decreaseQuantity(product)">-</button>
@@ -25,17 +32,17 @@
             <td>{{ product.price }} kr</td>
             <td>
               <button class="remove-btn" @click="removeFromCart(product)">
-                x
+                <b-icon icon="trash" font-scale="1.3"></b-icon>
               </button>
             </td>
           </tr>
         </tbody>
         <tbody>
           <tr>
-            <td colspan="4" v-if="this.products.length !== 0">
+            <td colspan="5" v-if="this.products.length !== 0">
               Totala summan: {{ totalSum }}kr
             </td>
-            <td colspan="4" v-else>Kundvagnen är tom</td>
+            <td colspan="5" v-else>Kundvagnen är tom</td>
           </tr>
         </tbody>
       </table>
@@ -58,168 +65,184 @@
 </template>
 
 <script>
-export default {
-  name: "Cart",
-  created() {
-    this.products = this.$store.state.cart;
-  },
-  data() {
-    return {
-      products: null,
-    };
-  },
-  methods: {
-    increaseQuantity(product) {
-      product.quantity++;
+  export default {
+    name: 'Cart',
+    created() {
+      this.products = this.$store.state.cart
     },
-    decreaseQuantity(product) {
-      if (product.quantity !== 0) {
-        product.quantity--;
-      }
-      if (product.quantity === 0) {
-        this.removeFromCart(product);
+    data() {
+      return {
+        products: null
       }
     },
-    removeFromCart(product) {
-      this.$store.commit("removeFromCart", product);
-    },
-  },
-  computed: {
-    totalSum() {
-      var sum = 0;
-      for (let i = 0; i < this.products.length; i++) {
-        sum += this.products[i].price * this.products[i].quantity;
+    methods: {
+      increaseQuantity(product) {
+        product.quantity++
+      },
+      decreaseQuantity(product) {
+        if (product.quantity !== 0) {
+          product.quantity--
+        }
+        if (product.quantity === 0) {
+          this.removeFromCart(product)
+        }
+      },
+      removeFromCart(product) {
+        this.$store.commit('removeFromCart', product)
       }
-      return sum;
     },
-  },
-};
+    computed: {
+      totalSum() {
+        var sum = 0
+        for (let i = 0; i < this.products.length; i++) {
+          sum += this.products[i].price * this.products[i].quantity
+        }
+        return sum
+      }
+    }
+  }
 </script>
 
 <style scoped>
-.cart-page {
-  padding-top: 100px;
-  width: 100%;
-}
-
-.cart {
-  overflow-x: auto;
-  margin: auto;
-  width: 100%;
-}
-
-.table {
-  width: 90%;
-  margin: 10px auto 20px auto;
-  color: #000000;
-  box-shadow: #00000050 1px 2px 4px;
-  border-radius: 5px;
-  background-color: #ffffff;
-}
-
-.table > thead {
-  background-color: #0000001a;
-}
-
-.table td {
-  vertical-align: middle;
-}
-
-.table-item:hover {
-  background-color: rgba(202, 202, 202, 0.24);
-}
-
-.product-quantity {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.product-quantity p {
-  margin: 0;
-  padding: 0 10px;
-  align-items: center;
-}
-
-.product-quantity > button {
-  border: none;
-  border-radius: 0.25rem;
-  background-color: #d1d1d19a;
-  width: 2em;
-  height: 2em;
-  box-shadow: #00000040 1px 1px 4px;
-}
-
-.remove-btn {
-  border: none;
-  width: 2.2em;
-  height: 2.2em;
-  border-radius: 5px;
-  background-color: #ff0000e5;
-  box-shadow: #00000040 1px 1px 4px;
-}
-
-.remove-btn:hover,
-.remove-btn:focus {
-  background-color: #c00404e5;
-}
-
-.checkout {
-  position: absolute;
-  left: 50%;
-  width: 90%;
-  transform: translateX(-50%);
-  border: none;
-  color: #ffffff;
-  padding: 1em;
-  border-radius: 5px;
-  background-color: #42b983;
-  box-shadow: #00000050 1px 2px 4px;
-}
-
-.checkout-disabled {
-  position: absolute;
-  left: 50%;
-  width: 90%;
-  transform: translateX(-50%);
-  border: none;
-  color: #ffffff;
-  padding: 1em;
-  border-radius: 5px;
-  background-color: #42b9835d;
-  box-shadow: #00000050 1px 2px 4px;
-}
-
-.checkout:hover,
-.checkout:focus {
-  text-decoration: none;
-  background-color: #289061;
-}
-
-@media screen and (min-width: 513px) and (max-width: 899px) {
-  .cart {
-    display: block;
-    margin: auto;
-    width: 512px;
+  .cart-page {
+    padding-top: 100px;
+    width: 100%;
   }
 
-  .checkout,
-  .checkout-disabled {
+  .cart {
+    overflow-x: auto;
+    margin: auto;
+    width: 100%;
+  }
+
+  .table {
+    width: 90%;
+    margin: 10px auto 20px auto;
+    color: #000000;
+    box-shadow: #00000050 1px 2px 4px;
+    border-radius: 5px;
+    background-color: #ffffff;
+  }
+
+  .table > thead {
+    background-color: #0000001a;
+  }
+
+  .table td {
+    vertical-align: middle;
+  }
+
+  .table-item:hover {
+    background-color: rgba(202, 202, 202, 0.24);
+  }
+
+  .table-item > td > img {
+    display: none;
+  }
+
+  .product-quantity {
     display: flex;
     justify-content: center;
-    position: relative;
-    width: 200px;
-  }
-}
-
-@media screen and (min-width: 900px) {
-  .cart {
-    width: 700px;
+    align-items: center;
   }
 
-  .checkout,
+  .product-quantity p {
+    margin: 0;
+    padding: 0 10px;
+    align-items: center;
+  }
+
+  .product-quantity > button {
+    border: 1px solid black;
+    border-radius: 0.25rem;
+    width: 1.6em;
+    height: 1.6em;
+    background-color: #ffffff;
+  }
+
+  .product-quantity > button:hover {
+    background-color: rgb(233, 233, 233);
+  }
+
+  .remove-btn {
+    border: 1px solid black;
+    padding: 5px 0 0 0;
+    width: 2em;
+    height: 2em;
+    border-radius: 0.25rem;
+    background-color: #ffffff;
+  }
+
+  .remove-btn:hover,
+  .remove-btn:focus {
+    background-color: #db0d0d;
+    color: #ffffff;
+  }
+
+  .checkout {
+    position: absolute;
+    left: 50%;
+    width: 90%;
+    transform: translateX(-50%);
+    border: none;
+    color: #ffffff;
+    padding: 1em;
+    border-radius: 5px;
+    background-color: #42b983;
+    box-shadow: #00000050 1px 2px 4px;
+  }
+
   .checkout-disabled {
-    width: 200px;
+    position: absolute;
+    left: 50%;
+    width: 90%;
+    transform: translateX(-50%);
+    border: none;
+    color: #ffffff;
+    padding: 1em;
+    border-radius: 5px;
+    background-color: #42b9835d;
+    box-shadow: #00000050 1px 2px 4px;
   }
-}
+
+  .checkout:hover,
+  .checkout:focus {
+    text-decoration: none;
+    background-color: #289061;
+  }
+
+  @media screen and (min-width: 513px) and (max-width: 900px) {
+    .cart {
+      display: block;
+      margin: auto;
+      width: 512px;
+    }
+
+    .table-item > td > img {
+      display: inline;
+    }
+
+    .checkout,
+    .checkout-disabled {
+      display: flex;
+      justify-content: center;
+      position: relative;
+      width: 200px;
+    }
+  }
+
+  @media screen and (min-width: 900px) {
+    .cart {
+      width: 700px;
+    }
+
+    .table-item > td > img {
+      display: inline;
+    }
+
+    .checkout,
+    .checkout-disabled {
+      width: 200px;
+    }
+  }
 </style>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -9,7 +9,7 @@
       <table class="table is-striped">
         <thead>
           <tr>
-            <th>Namn</th>
+            <th>Produkt</th>
             <th>Antal</th>
             <th>Pris</th>
           </tr>
@@ -94,316 +94,318 @@
 </template>
 
 <script>
-import emailjs from "emailjs-com";
+  import emailjs from 'emailjs-com'
 
-import { init } from "emailjs-com";
-init("user_TiTSwezkNDuf6XB9cHtHU");
+  import { init } from 'emailjs-com'
+  init('user_TiTSwezkNDuf6XB9cHtHU')
 
-export default {
-  name: "Checkout",
-  created() {
-    this.products = this.$store.state.cart;
-  },
-  data() {
-    return {
-      products: null,
-      firstName: "",
-      lastName: "",
-      streetAdress: "",
-      zipCode: "",
-      county: "",
-      email: "",
-      phoneNumber: "",
-      paymentMethod: "Kortbetalning",
-      cardNumber: "",
-      showCartInfo: false,
-    };
-  },
-  methods: {
-    showCart() {
-      this.showCartInfo = !this.showCartInfo;
+  export default {
+    name: 'Checkout',
+    created() {
+      this.products = this.$store.state.cart
     },
-    finishCheckout() {
-      var templateParams = {
-        fullName: this.fullName,
-        userEmail: this.email,
-        products: this.productsOrdered,
-        price: this.totalSum,
-        streetAdress: this.streetAdress,
-        zipCode: this.zipCode,
-        city: this.county,
-        phoneNumber: this.phoneNumber,
-        cardNumber: this.hiddenCardNumber,
-        paymentMethod: this.paymentMethod,
-        orderNr: this.randomizedOrderNumber,
-      };
-      emailjs.send("service_c3b8enq", "template_35snhib", templateParams).then(
-        function(response) {
-          console.log("SUCCESS!", response.status, response.text);
-        },
-        function(error) {
-          console.log("FAILED...", error);
+    data() {
+      return {
+        products: null,
+        firstName: '',
+        lastName: '',
+        streetAdress: '',
+        zipCode: '',
+        county: '',
+        email: '',
+        phoneNumber: '',
+        paymentMethod: 'Kortbetalning',
+        cardNumber: '',
+        showCartInfo: false
+      }
+    },
+    methods: {
+      showCart() {
+        this.showCartInfo = !this.showCartInfo
+      },
+      finishCheckout() {
+        var templateParams = {
+          fullName: this.fullName,
+          userEmail: this.email,
+          products: this.productsOrdered,
+          price: this.totalSum,
+          streetAdress: this.streetAdress,
+          zipCode: this.zipCode,
+          city: this.county,
+          phoneNumber: this.phoneNumber,
+          cardNumber: this.hiddenCardNumber,
+          paymentMethod: this.paymentMethod,
+          orderNr: this.randomizedOrderNumber
         }
-      );
-      this.clearInputsAndCart();
-    },
-    clearInputsAndCart() {
-      this.$store.commit("clearCart");
-      this.firstName = "";
-      this.lastName = "";
-      this.email = "";
-      this.products = this.$store.state.cart;
-      this.streetAdress = "";
-      this.zipCode = "";
-      this.county = "";
-      this.phoneNumber = "";
-      this.cardNumber = "";
-    },
-  },
-  computed: {
-    fullName() {
-      return this.firstName + " " + this.lastName;
-    },
-    adress() {
-      return this.streetAdress + "\n" + this.zipCode + " " + this.county;
-    },
-    totalSum() {
-      var sum = 0;
-      for (let i = 0; i < this.products.length; i++) {
-        sum += this.products[i].price * this.products[i].quantity;
+        emailjs
+          .send('service_c3b8enq', 'template_35snhib', templateParams)
+          .then(
+            function(response) {
+              console.log('SUCCESS!', response.status, response.text)
+            },
+            function(error) {
+              console.log('FAILED...', error)
+            }
+          )
+        this.clearInputsAndCart()
+      },
+      clearInputsAndCart() {
+        this.$store.commit('clearCart')
+        this.firstName = ''
+        this.lastName = ''
+        this.email = ''
+        this.products = this.$store.state.cart
+        this.streetAdress = ''
+        this.zipCode = ''
+        this.county = ''
+        this.phoneNumber = ''
+        this.cardNumber = ''
       }
-      return sum;
     },
-    randomizedOrderNumber() {
-      return Math.floor(100000 + Math.random() * 900000);
-    },
-    hiddenCardNumber() {
-      var shortCcNum = "";
-      for (
-        var i = this.cardNumber.length - 4;
-        i < this.cardNumber.length;
-        i++
-      ) {
-        shortCcNum += this.cardNumber.charAt(i);
+    computed: {
+      fullName() {
+        return this.firstName + ' ' + this.lastName
+      },
+      adress() {
+        return this.streetAdress + '\n' + this.zipCode + ' ' + this.county
+      },
+      totalSum() {
+        var sum = 0
+        for (let i = 0; i < this.products.length; i++) {
+          sum += this.products[i].price * this.products[i].quantity
+        }
+        return sum
+      },
+      randomizedOrderNumber() {
+        return Math.floor(100000 + Math.random() * 900000)
+      },
+      hiddenCardNumber() {
+        var shortCcNum = ''
+        for (
+          var i = this.cardNumber.length - 4;
+          i < this.cardNumber.length;
+          i++
+        ) {
+          shortCcNum += this.cardNumber.charAt(i)
+        }
+        return shortCcNum
+      },
+      productsOrdered() {
+        var orderedProductsHTML = []
+        for (let i = 0; i < this.products.length; i++) {
+          orderedProductsHTML.push(
+            '<tr style="height: 24px;">' +
+              '<td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;" align="left" width="75%">' +
+              this.products[i].name +
+              ' x' +
+              this.products[i].quantity +
+              '</td>' +
+              '<td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;" align="left" width="25%">' +
+              this.products[i].price +
+              ' kr' +
+              '</td>' +
+              '<tr>'
+          )
+        }
+        return orderedProductsHTML.join('\n')
       }
-      return shortCcNum;
-    },
-    productsOrdered() {
-      var orderedProductsHTML = [];
-      for (let i = 0; i < this.products.length; i++) {
-        orderedProductsHTML.push(
-          '<tr style="height: 24px;">' +
-            '<td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;" align="left" width="75%">' +
-            this.products[i].name +
-            " x" +
-            this.products[i].quantity +
-            "</td>" +
-            '<td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;" align="left" width="25%">' +
-            this.products[i].price +
-            " kr" +
-            "</td>" +
-            "<tr>"
-        );
-      }
-      return orderedProductsHTML.join("\n");
-    },
-  },
-};
+    }
+  }
 </script>
 
 <style scoped>
-.cart-page {
-  padding-top: 100px;
-}
+  .cart-page {
+    padding-top: 100px;
+  }
 
-.show-cart-btn {
-  margin-top: 10px;
-  margin-bottom: 20px;
-  padding: 5px 10px 5px 10px;
-  width: 150px;
-  border-radius: 5px;
-  border: none;
-  background-color: #00000025;
-}
+  .show-cart-btn {
+    margin-top: 10px;
+    margin-bottom: 20px;
+    padding: 5px 10px 5px 10px;
+    width: 150px;
+    border-radius: 5px;
+    border: none;
+    background-color: #00000025;
+  }
 
-.show-cart-btn p {
-  margin: 0 auto;
-}
+  .show-cart-btn p {
+    margin: 0 auto;
+  }
 
-.show-cart-btn:hover {
-  background-color: #00000030;
-}
+  .show-cart-btn:hover {
+    background-color: #00000030;
+  }
 
-.cart-info {
-  box-shadow: #0000001f 1px 2px 4px;
-  background-color: #ffffff;
-  display: block;
-  margin: 4px auto 0 auto;
-  width: 90%;
-  border-radius: 5px;
-  overflow-x: auto;
-}
-
-.table {
-  color: #000;
-  margin: 0;
-}
-
-.table > thead {
-  background-color: #0000001a;
-}
-
-.input-wrapper {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin: 25px auto 0 auto;
-  /* border: 1px solid black */
-}
-
-.inputs {
-  display: grid;
-  grid-template-columns: auto;
-  /* border: 1px red solid; */
-  width: 90%;
-  justify-items: start;
-}
-
-.payment-header {
-  display: block;
-  margin: 20px auto;
-}
-.payment-header h6 {
-  font-size: 1.4rem;
-}
-
-.grid-item {
-  position: relative;
-  /* border: 1px blue solid; */
-  width: 100%;
-  padding: 0;
-  margin-bottom: 10px;
-}
-
-.grid-item > label {
-  position: absolute;
-  left: 10px;
-}
-
-.grid-item > input {
-  border: none;
-  margin: 0 0 5px 0px;
-  padding: 10px 15px;
-  border-radius: 5px;
-  width: 100%;
-  box-shadow: #0000001f 1px 2px 4px;
-}
-
-.price-total {
-  margin-top: 10px;
-  font-weight: bold;
-  font-size: 1.1em;
-}
-
-.buttons {
-  display: inline-block;
-  width: 100%;
-  margin-bottom: 20px;
-}
-
-.cancel-btn,
-.checkout-btn {
-  border: none;
-  height: 100%;
-  border-radius: 5px;
-  padding: 1em;
-  margin: 0 5px;
-}
-
-.cancel-btn {
-  background-color: rgba(245, 21, 21, 0.815);
-  color: #000000;
-}
-
-.cancel-btn:hover,
-.cancel-btn:focus {
-  text-decoration: none;
-  background-color: rgba(197, 8, 8, 0.787);
-}
-
-.checkout-btn {
-  background-color: #42b983;
-}
-
-.checkout-btn:hover,
-.checkout-btn:focus {
-  background-color: #289061;
-}
-
-@media screen and (min-width: 513px) {
   .cart-info {
-    width: 480px;
+    box-shadow: #0000001f 1px 2px 4px;
+    background-color: #ffffff;
+    display: block;
+    margin: 4px auto 0 auto;
+    width: 90%;
+    border-radius: 5px;
+    overflow-x: auto;
+  }
+
+  .table {
+    color: #000;
+    margin: 0;
+  }
+
+  .table > thead {
+    background-color: #0000001a;
+  }
+
+  .input-wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin: 25px auto 0 auto;
+    /* border: 1px solid black */
   }
 
   .inputs {
-    grid-template-columns: auto auto;
-    grid-gap: 10px;
-    width: 480px;
+    display: grid;
+    grid-template-columns: auto;
+    /* border: 1px red solid; */
+    width: 90%;
+    justify-items: start;
   }
 
-  .item1 {
-    grid-column: 1;
-    grid-row: 1;
-  }
-
-  .item2 {
-    grid-column: 2;
-    grid-row: 1;
-  }
-
-  .item3 {
-    grid-column: 1 / span 2;
-    grid-row: 2;
-  }
-
-  .item4 {
-    grid-column: 1;
-    grid-row: 3;
-  }
-
-  .item5 {
-    grid-column: 2;
-    grid-row: 3;
-  }
-
-  .item6 {
-    grid-column: 1 / span 2;
-    grid-row: 4;
-  }
-
-  .item7 {
-    grid-column: 1 / span 2;
-    grid-row: 5;
-  }
   .payment-header {
-    grid-column: 1 / span 2;
-    grid-row: 6;
+    display: block;
+    margin: 20px auto;
   }
-  .item8 {
-    grid-column: 1 / span 2;
-    grid-row: 7;
-  }
-}
-
-@media screen and (min-width: 900px) {
-  .cart-info {
-    width: 600px;
+  .payment-header h6 {
+    font-size: 1.4rem;
   }
 
-  .inputs {
-    grid-template-columns: auto auto;
-    grid-gap: 10px;
-    width: 500px;
+  .grid-item {
+    position: relative;
+    /* border: 1px blue solid; */
+    width: 100%;
+    padding: 0;
+    margin-bottom: 10px;
   }
-}
+
+  .grid-item > label {
+    position: absolute;
+    left: 10px;
+  }
+
+  .grid-item > input {
+    border: none;
+    margin: 0 0 5px 0px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    width: 100%;
+    box-shadow: #0000001f 1px 2px 4px;
+  }
+
+  .price-total {
+    margin-top: 10px;
+    font-weight: bold;
+    font-size: 1.1em;
+  }
+
+  .buttons {
+    display: inline-block;
+    width: 100%;
+    margin-bottom: 20px;
+  }
+
+  .cancel-btn,
+  .checkout-btn {
+    border: none;
+    height: 100%;
+    border-radius: 5px;
+    padding: 1em;
+    margin: 0 5px;
+  }
+
+  .cancel-btn {
+    background-color: rgba(245, 21, 21, 0.815);
+    color: #000000;
+  }
+
+  .cancel-btn:hover,
+  .cancel-btn:focus {
+    text-decoration: none;
+    background-color: rgba(197, 8, 8, 0.787);
+  }
+
+  .checkout-btn {
+    background-color: #42b983;
+  }
+
+  .checkout-btn:hover,
+  .checkout-btn:focus {
+    background-color: #289061;
+  }
+
+  @media screen and (min-width: 513px) {
+    .cart-info {
+      width: 480px;
+    }
+
+    .inputs {
+      grid-template-columns: auto auto;
+      grid-gap: 10px;
+      width: 480px;
+    }
+
+    .item1 {
+      grid-column: 1;
+      grid-row: 1;
+    }
+
+    .item2 {
+      grid-column: 2;
+      grid-row: 1;
+    }
+
+    .item3 {
+      grid-column: 1 / span 2;
+      grid-row: 2;
+    }
+
+    .item4 {
+      grid-column: 1;
+      grid-row: 3;
+    }
+
+    .item5 {
+      grid-column: 2;
+      grid-row: 3;
+    }
+
+    .item6 {
+      grid-column: 1 / span 2;
+      grid-row: 4;
+    }
+
+    .item7 {
+      grid-column: 1 / span 2;
+      grid-row: 5;
+    }
+    .payment-header {
+      grid-column: 1 / span 2;
+      grid-row: 6;
+    }
+    .item8 {
+      grid-column: 1 / span 2;
+      grid-row: 7;
+    }
+  }
+
+  @media screen and (min-width: 900px) {
+    .cart-info {
+      width: 600px;
+    }
+
+    .inputs {
+      grid-template-columns: auto auto;
+      grid-gap: 10px;
+      width: 500px;
+    }
+  }
 </style>
