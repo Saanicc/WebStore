cd1e6a49042aba682ce79a008a7a258007a554e3
ChabbeH <72022949+ChabbeH@users.noreply.github.com>
2021-05-31 00:30:38 +0200

    Implementerat vuelidate
    Input validering

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import Vuelidate from 'vuelidate'
 
-import "./main.scss";
+import './main.scss'
 
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import VShowSlide from "v-show-slide";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VShowSlide from 'v-show-slide'
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-Vue.use(VShowSlide);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VShowSlide)
+Vue.use(Vuelidate)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -26,33 +26,97 @@
 
     <div class="input-wrapper">
       <div class="inputs">
-        <div class="grid-item item1">
+        <div
+          class="grid-item item1"
+          :class="{
+            error: $v.firstName.$error
+          }"
+        >
           <label>Förnamn</label><br />
-          <input name="firstName" type="text" v-model="firstName" />
+          <input name="firstName" type="text" v-model="$v.firstName.$model" />
+          <div class="error-label" v-if="!$v.firstName.required">
+            Fyll i ditt namn
+          </div>
+          <div class="error-label" v-if="!$v.firstName.minLength">
+            Måste ha {{ $v.firstName.$params.minLength.min }} bokstäver
+          </div>
         </div>
-        <div class="grid-item item2">
+        <div
+          class="grid-item item2"
+          :class="{
+            error: $v.lastName.$error
+          }"
+        >
           <label>Efternamn</label><br />
-          <input name="lastName" type="text" v-model="lastName" />
+          <input name="lastName" type="text" v-model="$v.lastName.$model" />
+          <div class="error-label" v-if="!$v.lastName.required">
+            Fyll i ditt efternamn
+          </div>
+          <div class="error-label" v-if="!$v.lastName.minLength">
+            Måste ha {{ $v.firstName.$params.minLength.min }} bokstäver
+          </div>
         </div>
-        <div class="grid-item item3">
+        <div
+          class="grid-item item3"
+          :class="{
+            error: $v.streetAdress.$error
+          }"
+        >
           <label>Gatuadress</label><br />
-          <input type="text" v-model="streetAdress" />
+          <input
+            name="streetAdress"
+            type="text"
+            v-model="$v.streetAdress.$model"
+          />
+          <div class="error" v-if="!$v.streetAdress.required">
+            Du måste fylla i din adress
+          </div>
         </div>
-        <div class="grid-item item4">
+        <div class="grid-item item4" :class="{ error: $v.zipCode.$error }">
           <label>Postnummer</label><br />
-          <input type="text" v-model="zipCode" />
+          <input type="text" v-model="$v.zipCode.$model" />
+          <div class="error-label" v-if="!$v.zipCode.required.numeric">
+            Fyll i ditt postnummer
+          </div>
+          <div class="error-label" v-if="!$v.zipCode.minLength">
+            Postnumret måste innehålla
+            {{ $v.zipCode.$params.minLength.min }} siffror
+          </div>
         </div>
-        <div class="grid-item item5">
+        <div class="grid-item item5" :class="{ error: $v.county.$error }">
           <label>Ort</label><br />
-          <input type="text" v-model="county" />
+          <input type="text" v-model="$v.county.$model" />
+          <div class="error" v-if="!$v.county.required">
+            Du måste fylla i din ort
+          </div>
         </div>
-        <div class="grid-item item6">
+        <div
+          class="grid-item item6"
+          :class="{
+            error: $v.mail.$error
+          }"
+        >
           <label>Email</label><br />
-          <input name="user_email" type="text" v-model="email" />
+          <input name="user_email" type="text" v-model="$v.mail.$model" />
+          <div class="error" v-if="!$v.mail.required.email">
+            Fyll i din email
+          </div>
+          <div class="error" v-if="!$v.mail"></div>
         </div>
-        <div class="grid-item item7">
+        <div
+          class="grid-item item7"
+          :class="{
+            error: $v.phoneNumber.$error
+          }"
+        >
           <label>Telefonnummer</label><br />
-          <input type="text" v-model="phoneNumber" />
+          <input type="text" v-model="$v.phoneNumber.$model" />
+          <div class="error" v-if="!$v.phoneNumber.required.numeric">
+            Fyll i ditt nummer
+          </div>
+          <div class="error" v-if="!$v.phoneNumber.minLength">
+            Måste ha {{ $v.phoneNumber.$params.minLength.min }} siffror
+          </div>
         </div>
       </div>
     </div>
@@ -75,31 +139,103 @@
     </div>
     <div class="input-wrapper">
       <div class="payment-inputs" v-if="paymentMethod === 'CARDPAYMENT'">
-        <div class="grid-item item8">
+        <div
+          class="grid-item item8"
+          :class="{
+            error: $v.cardNumber.$error
+          }"
+        >
           <label>Kortnummer</label><br />
-          <input type="text" v-model="cardNumber" />
+          <input type="text" v-model="$v.cardNumber.$model" />
+          <div class="error" v-if="!$v.cardNumber.numeric">
+            Måste innehålla siffror
+          </div>
+          <div class="error" v-if="!$v.cardNumber.minLength">
+            Måste ha {{ $v.cardNumber.$params.minLength.min }} siffror
+          </div>
+          <div class="error" v-if="!$v.cardNumber.maxLength">
+            Får max {{ $v.cardNumber.$params.maxLength.max }} siffror
+          </div>
         </div>
-        <div class="grid-item item9">
+        <div
+          class="grid-item item9"
+          :class="{
+            error: $v.cardMonth.$error
+          }"
+        >
           <label>Månad</label><br />
-          <input type="text" />
+          <input type="text" v-model="$v.cardMonth.$model" />
+          <div class="error" v-if="!$v.cardMonth.numeric">
+            Måste innehålla siffror
+          </div>
+          <div class="error" v-if="!$v.cardMonth.maxLength">
+            Får max ha {{ $v.cardMonth.$params.maxLength.max }} siffror
+          </div>
         </div>
-        <div class="grid-item item10">
+        <div
+          class="grid-item item10"
+          :class="{
+            error: $v.cardYear.$error
+          }"
+        >
           <label>År</label><br />
-          <input type="text" />
+          <input type="text" v-model="$v.cardYear.$model" />
+          <div class="error" v-if="!$v.cardYear.numeric">
+            Måste innehålla siffror
+          </div>
+          <div class="error" v-if="!$v.cardYear.minLength">
+            Måste ha minst {{ $v.cardYear.$params.minLength.min }} siffror
+          </div>
+          <div class="error" v-if="!$v.cardYear.maxLength">
+            Får max ha {{ $v.cardYear.$params.maxLength.max }} siffror
+          </div>
         </div>
-        <div class="grid-item item11">
+        <div
+          class="grid-item item11"
+          :class="{
+            error: $v.cvc.$error
+          }"
+        >
           <label>CVC2</label><br />
-          <input type="text" />
+          <input type="text" v-model="$v.cvc.$model" />
+          <div class="error" v-if="!$v.cvc.numeric">
+            Måste innehålla siffror
+          </div>
+          <div class="error" v-if="!$v.cvc.minLength">
+            Måste ha minst {{ $v.cvc.$params.minLength.min }} siffror
+          </div>
+          <div class="error" v-if="!$v.cvc.maxLength">
+            Får max ha {{ $v.cvc.$params.maxLength.max }} siffror
+          </div>
         </div>
-        <div class="grid-item item12">
+        <div
+          class="grid-item item12"
+          :class="{
+            error: $v.cardHolder.$error
+          }"
+        >
           <label>Kortinnehavare</label><br />
-          <input type="text" />
+          <input type="text" v-model="$v.cardHolder.$model" />
+          <div class="error" v-if="!$v.cardHolder.numeric">
+            Skriv ditt namn
+          </div>
         </div>
       </div>
       <div class="payment-inputs" v-if="paymentMethod === 'SWISH'">
-        <div class="grid-item item8">
+        <div
+          class="grid-item item8"
+          :class="{
+            error: $v.swishNumber.$error
+          }"
+        >
           <label>Telefonnummer</label><br />
-          <input type="text" v-model="swishNumber" />
+          <input type="text" v-model="$v.swishNumber.$model" />
+          <div class="error" v-if="!$v.swishNumber.numeric">
+            Måste innehålla siffror
+          </div>
+          <div class="error" v-if="!$v.swishNumber.minLength">
+            Måste innehålla {{ $v.swishNumber.$params.minLength.min }} siffror
+          </div>
         </div>
       </div>
     </div>
@@ -123,6 +259,14 @@
   import { init } from 'emailjs-com'
   init('user_TiTSwezkNDuf6XB9cHtHU')
 
+  import {
+    required,
+    minLength,
+    numeric,
+    maxLength,
+    email
+  } from 'vuelidate/lib/validators'
+
   export default {
     name: 'Checkout',
     created() {
@@ -136,15 +280,81 @@
         streetAdress: '',
         zipCode: '',
         county: '',
-        email: '',
+        mail: '',
         phoneNumber: '',
         paymentMethod: '',
         cardNumber: '',
         showCartInfo: false,
         payment: 'Välj betalningsalternativ',
-        swishNumber: ''
+        swishNumber: '',
+        cardMonth: '',
+        cardYear: '',
+        cvc: '',
+        cardHolder: ''
       }
     },
+    validations: {
+      firstName: {
+        required,
+        minLength: minLength(2)
+      },
+      lastName: {
+        required,
+        minLength: minLength(2)
+      },
+      cardNumber: {
+        required,
+        numeric,
+        minLength: minLength(16),
+        maxLength: maxLength(16)
+      },
+      mail: {
+        required,
+        email
+      },
+      phoneNumber: {
+        required,
+        numeric,
+        minLength: minLength(10)
+      },
+      streetAdress: {
+        required
+      },
+      county: {
+        required
+      },
+      zipCode: {
+        required,
+        numeric,
+        minLength: minLength(5)
+      },
+      cardMonth: {
+        required,
+        numeric,
+        maxLength: maxLength(2)
+      },
+      cardYear: {
+        required,
+        numeric,
+        minLength: minLength(2),
+        maxLength: maxLength(4)
+      },
+      cvc: {
+        required,
+        numeric,
+        minLength: minLength(3),
+        maxLength: maxLength(3)
+      },
+      cardHolder: {
+        required
+      },
+      swishNumber: {
+        required,
+        numeric,
+        minLength: minLength(10)
+      }
+    },
+
     methods: {
       cardPayment() {
         this.paymentMethod = 'CARDPAYMENT'
@@ -161,7 +371,7 @@
       finishCheckout() {
         var templateParams = {
           fullName: this.fullName,
-          userEmail: this.email,
+          userEmail: this.mail,
           products: this.productsOrdered,
           price: this.totalSum,
           streetAdress: this.streetAdress,
@@ -189,7 +399,7 @@
         this.$store.commit('clearCart')
         this.firstName = ''
         this.lastName = ''
-        this.email = ''
+        this.mail = ''
         this.products = this.$store.state.cart
         this.streetAdress = ''
         this.zipCode = ''
@@ -311,6 +521,19 @@
     grid-template-columns: auto;
     /* border: 1px red solid; */
     width: 90%;
+  }
+  .error > label {
+    color: #db0d0d;
+  }
+  .error > div {
+    color: #db0d0d;
+  }
+  .error > input {
+    border: none;
+    border: 1px solid #db0d0d !important;
+  }
+  .error-input {
+    outline: 1px solid #db0d0d;
   }
 
   .payment-header {
